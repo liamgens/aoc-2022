9165003f0db4e3d92754af7aa1710f55a6d422e7
Liam Gensel <liam.gensel@gocrisp.com>
2022-12-05 22:41:03 -0700

    move

--- src/main/kotlin/Utils.kt
+++
@@ -1,4 +0,0 @@
-fun <T> ArrayDeque<T>.peek() = this.removeLastOrNull()
-fun <T> ArrayDeque<T>.push(element: T) = addLast(element)
-fun <T> ArrayDeque<T>.push(elements: List<T>) = elements.forEach { addLast(it) }
-fun <T> ArrayDeque<T>.pop(count: Int): List<T> = (0 until count).map { this.removeLast() }
